db5f34ffe863c11608635bfd72014c97591aeb83
user <user@DESK-HP>
2022-01-24 20:19:20 +0530

    user1 second commit

--- src/test/java/demoGit/DemoGit.java
+++ src/test/java/demoGit/DemoGit.java
@@ -3,8 +3,8 @@ package demoGit;
 public class DemoGit {
 
 	public static void main(String[] args) {
-		System.out.println("git hub push");
-
+		System.out.println("user1 first commit git hub push");
+		System.out.println("user1 second commit");
 	}
 
 }
